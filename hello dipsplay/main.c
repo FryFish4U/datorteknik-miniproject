7e81c9103c514ec8449a796383c0748ab1a86901
Chef-mux <ddmh89@gmail.com>
2023-02-24 14:09:10 +0100

    Code for project
    main file with display, sprite file with images and some other files that i have not touched

---
+++ hello dipsplay/main.c
@@ -0,0 +1,359 @@
+#include <pic32mx.h>
+#include <stdint.h>
+//#include <sprites>
+
+#define DISPLAY_VDD PORTFbits.RF6
+#define DISPLAY_VBATT PORTFbits.RF5
+#define DISPLAY_COMMAND_DATA PORTFbits.RF4
+#define DISPLAY_RESET PORTGbits.RG9
+
+
+#define DISPLAY_VDD_PORT PORTF
+#define DISPLAY_VDD_MASK 0x40
+#define DISPLAY_VBATT_PORT PORTF
+#define DISPLAY_VBATT_MASK 0x20
+#define DISPLAY_COMMAND_DATA_PORT PORTF
+#define DISPLAY_COMMAND_DATA_MASK 0x10
+#define DISPLAY_RESET_PORT PORTG
+#define DISPLAY_RESET_MASK 0x200
+
+ gameMap[512];
+int lane = 0;	//make global in header	
+
+char textbuffer[4][16];
+
+static const uint8_t const font[] = {
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 94, 0, 0, 0, 0,
+	0, 0, 4, 3, 4, 3, 0, 0,
+	0, 36, 126, 36, 36, 126, 36, 0,
+	0, 36, 74, 255, 82, 36, 0, 0,
+	0, 70, 38, 16, 8, 100, 98, 0,
+	0, 52, 74, 74, 52, 32, 80, 0,
+	0, 0, 0, 4, 3, 0, 0, 0,
+	0, 0, 0, 126, 129, 0, 0, 0,
+	0, 0, 0, 129, 126, 0, 0, 0,
+	0, 42, 28, 62, 28, 42, 0, 0,
+	0, 8, 8, 62, 8, 8, 0, 0,
+	0, 0, 0, 128, 96, 0, 0, 0,
+	0, 8, 8, 8, 8, 8, 0, 0,
+	0, 0, 0, 0, 96, 0, 0, 0,
+	0, 64, 32, 16, 8, 4, 2, 0,
+	0, 62, 65, 73, 65, 62, 0, 0,
+	0, 0, 66, 127, 64, 0, 0, 0,
+	0, 0, 98, 81, 73, 70, 0, 0,
+	0, 0, 34, 73, 73, 54, 0, 0,
+	0, 0, 14, 8, 127, 8, 0, 0,
+	0, 0, 35, 69, 69, 57, 0, 0,
+	0, 0, 62, 73, 73, 50, 0, 0,
+	0, 0, 1, 97, 25, 7, 0, 0,
+	0, 0, 54, 73, 73, 54, 0, 0,
+	0, 0, 6, 9, 9, 126, 0, 0,
+	0, 0, 0, 102, 0, 0, 0, 0,
+	0, 0, 128, 102, 0, 0, 0, 0,
+	0, 0, 8, 20, 34, 65, 0, 0,
+	0, 0, 20, 20, 20, 20, 0, 0,
+	0, 0, 65, 34, 20, 8, 0, 0,
+	0, 2, 1, 81, 9, 6, 0, 0,
+	0, 28, 34, 89, 89, 82, 12, 0,
+	0, 0, 126, 9, 9, 126, 0, 0,
+	0, 0, 127, 73, 73, 54, 0, 0,
+	0, 0, 62, 65, 65, 34, 0, 0,
+	0, 0, 127, 65, 65, 62, 0, 0,
+	0, 0, 127, 73, 73, 65, 0, 0,
+	0, 0, 127, 9, 9, 1, 0, 0,
+	0, 0, 62, 65, 81, 50, 0, 0,
+	0, 0, 127, 8, 8, 127, 0, 0,
+	0, 0, 65, 127, 65, 0, 0, 0,
+	0, 0, 32, 64, 64, 63, 0, 0,
+	0, 0, 127, 8, 20, 99, 0, 0,
+	0, 0, 127, 64, 64, 64, 0, 0,
+	0, 127, 2, 4, 2, 127, 0, 0,
+	0, 127, 6, 8, 48, 127, 0, 0,
+	0, 0, 62, 65, 65, 62, 0, 0,
+	0, 0, 127, 9, 9, 6, 0, 0,
+	0, 0, 62, 65, 97, 126, 64, 0,
+	0, 0, 127, 9, 9, 118, 0, 0,
+	0, 0, 38, 73, 73, 50, 0, 0,
+	0, 1, 1, 127, 1, 1, 0, 0,
+	0, 0, 63, 64, 64, 63, 0, 0,
+	0, 31, 32, 64, 32, 31, 0, 0,
+	0, 63, 64, 48, 64, 63, 0, 0,
+	0, 0, 119, 8, 8, 119, 0, 0,
+	0, 3, 4, 120, 4, 3, 0, 0,
+	0, 0, 113, 73, 73, 71, 0, 0,
+	0, 0, 127, 65, 65, 0, 0, 0,
+	0, 2, 4, 8, 16, 32, 64, 0,
+	0, 0, 0, 65, 65, 127, 0, 0,
+	0, 4, 2, 1, 2, 4, 0, 0,
+	0, 64, 64, 64, 64, 64, 64, 0,
+	0, 0, 1, 2, 4, 0, 0, 0,
+	0, 0, 48, 72, 40, 120, 0, 0,
+	0, 0, 127, 72, 72, 48, 0, 0,
+	0, 0, 48, 72, 72, 0, 0, 0,
+	0, 0, 48, 72, 72, 127, 0, 0,
+	0, 0, 48, 88, 88, 16, 0, 0,
+	0, 0, 126, 9, 1, 2, 0, 0,
+	0, 0, 80, 152, 152, 112, 0, 0,
+	0, 0, 127, 8, 8, 112, 0, 0,
+	0, 0, 0, 122, 0, 0, 0, 0,
+	0, 0, 64, 128, 128, 122, 0, 0,
+	0, 0, 127, 16, 40, 72, 0, 0,
+	0, 0, 0, 127, 0, 0, 0, 0,
+	0, 120, 8, 16, 8, 112, 0, 0,
+	0, 0, 120, 8, 8, 112, 0, 0,
+	0, 0, 48, 72, 72, 48, 0, 0,
+	0, 0, 248, 40, 40, 16, 0, 0,
+	0, 0, 16, 40, 40, 248, 0, 0,
+	0, 0, 112, 8, 8, 16, 0, 0,
+	0, 0, 72, 84, 84, 36, 0, 0,
+	0, 0, 8, 60, 72, 32, 0, 0,
+	0, 0, 56, 64, 32, 120, 0, 0,
+	0, 0, 56, 64, 56, 0, 0, 0,
+	0, 56, 64, 32, 64, 56, 0, 0,
+	0, 0, 72, 48, 48, 72, 0, 0,
+	0, 0, 24, 160, 160, 120, 0, 0,
+	0, 0, 100, 84, 84, 76, 0, 0,
+	0, 0, 8, 28, 34, 65, 0, 0,
+	0, 0, 0, 126, 0, 0, 0, 0,
+	0, 0, 65, 34, 28, 8, 0, 0,
+	0, 0, 4, 2, 4, 2, 0, 0,
+	0, 120, 68, 66, 68, 120, 0, 0,
+};
+
+const uint8_t const icon[] = {
+	255, 255, 255, 255, 255, 255, 127, 187,
+	68, 95, 170, 93, 163, 215, 175, 95,
+	175, 95, 175, 95, 223, 111, 175, 247,
+	59, 237, 242, 254, 171, 254, 1, 255,
+	255, 255, 15, 211, 109, 58, 253, 8,
+	178, 77, 58, 199, 122, 197, 242, 173,
+	242, 237, 186, 215, 40, 215, 41, 214,
+	35, 175, 91, 212, 63, 234, 149, 111,
+	171, 84, 253, 252, 254, 253, 126, 184,
+	195, 52, 201, 22, 225, 27, 196, 19,
+	165, 74, 36, 146, 72, 162, 85, 8,
+	226, 25, 166, 80, 167, 216, 167, 88,
+	106, 149, 161, 95, 135, 91, 175, 87,
+	142, 123, 134, 127, 134, 121, 134, 121,
+	132, 59, 192, 27, 164, 74, 177, 70,
+	184, 69, 186, 69, 254, 80, 175, 217,
+};
+
+const uint8_t const ufo[] = {
+	239, 239, 199, 135, 137, 141, 158, 22,
+    146, 30, 150, 18, 158, 141, 137, 135,
+    199, 239, 239 
+	
+};
+
+void delay(int cyc) {
+	int i;
+	for(i = cyc; i > 0; i--);
+}
+
+uint8_t spi_send_recv(uint8_t data) {
+	while(!(SPI2STAT & 0x08));
+	SPI2BUF = data;
+	while(!(SPI2STAT & 0x01));
+	return SPI2BUF;
+}
+
+void display_init() {
+	DISPLAY_COMMAND_DATA_PORT &= ~DISPLAY_COMMAND_DATA_MASK;
+	delay(10);
+	DISPLAY_VDD_PORT &= ~DISPLAY_VDD_MASK;
+	delay(1000000);
+	
+	spi_send_recv(0xAE);
+	DISPLAY_RESET_PORT &= ~DISPLAY_RESET_MASK;
+	delay(10);
+	DISPLAY_RESET_PORT |= DISPLAY_RESET_MASK;
+	delay(10);
+	
+	spi_send_recv(0x8D);
+	spi_send_recv(0x14);
+	
+	spi_send_recv(0xD9);
+	spi_send_recv(0xF1);
+	
+	DISPLAY_VBATT_PORT &= ~DISPLAY_VBATT_MASK;
+	delay(10000000);
+	
+	spi_send_recv(0xA1);
+	spi_send_recv(0xC8);
+	
+	spi_send_recv(0xDA);
+	spi_send_recv(0x20);
+	
+	spi_send_recv(0xAF);
+}
+
+void display_string(int line, char *s) {
+	int i;
+	if(line < 0 || line >= 4)
+		return;
+	if(!s)
+		return;
+	
+	for(i = 0; i < 16; i++)
+		if(*s) {
+			textbuffer[line][i] = *s;
+			s++;
+		} else
+			textbuffer[line][i] = ' ';
+}
+
+void display_image(int x, const uint8_t *data) {
+	int i, j;
+	
+	for(i = 0; i < 4; i++) {
+		DISPLAY_COMMAND_DATA_PORT &= ~DISPLAY_COMMAND_DATA_MASK;
+		spi_send_recv(0x22);
+		spi_send_recv(i);
+		
+		spi_send_recv(x & 0xF);
+		spi_send_recv(0x10 | ((x >> 4) & 0xF));
+		
+		DISPLAY_COMMAND_DATA_PORT |= DISPLAY_COMMAND_DATA_MASK;
+		
+		for(j = 0; j < 128; j++)
+			spi_send_recv(~data[i*128 + j]);
+	}
+}
+
+void display_update() {
+	int i, j, k;
+	int c;
+	for(i = 0; i < 4; i++) {
+		DISPLAY_COMMAND_DATA_PORT &= ~DISPLAY_COMMAND_DATA_MASK;
+		spi_send_recv(0x22);
+		spi_send_recv(i);
+		
+		spi_send_recv(0x0);
+		spi_send_recv(0x10);
+		
+		DISPLAY_COMMAND_DATA_PORT |= DISPLAY_COMMAND_DATA_MASK;
+		
+		for(j = 0; j < 16; j++) {
+			c = textbuffer[i][j];
+			if(c & 0x80)
+				continue;
+			
+			for(k = 0; k < 8; k++)
+				spi_send_recv(font[c*8 + k]);
+		}
+	}
+}
+
+int main(void) {
+	/* Set up peripheral bus clock */
+	OSCCON &= ~0x180000;
+	OSCCON |= 0x080000;
+	
+	/* Set up output pins */
+	AD1PCFG = 0xFFFF;
+	ODCE = 0x0;
+	TRISECLR = 0xFF;
+	PORTE = 0x0;
+	
+	/* Output pins for display signals */
+	PORTF = 0xFFFF;
+	PORTG = (1 << 9);
+	ODCF = 0x0;
+	ODCG = 0x0;
+	TRISFCLR = 0x70;
+	TRISGCLR = 0x200;
+	
+	/* Set up input pins */
+	TRISDSET = (1 << 8);
+	TRISFSET = (1 << 1);
+	
+	/* Set up SPI as master */
+	SPI2CON = 0;
+	SPI2BRG = 4;
+	
+	/* Clear SPIROV*/
+	SPI2STATCLR &= ~0x40;
+	/* Set CKP = 1, MSTEN = 1; */
+        SPI2CON |= 0x60;
+	
+	/* Turn on SPI */
+	SPI2CONSET = 0x8000;
+	
+	display_init();
+	// display_string(0, "such world");
+	// display_string(2, "much hello");
+	// display_string(3, "many text");
+	// display_string(, "wow");
+
+
+	{
+		/* code */
+	}
+	
+	int i = 0;
+	// while(1){
+	// while (i < (128-32)){
+	// 	display_image(i, ufo);
+	// 	i++;
+	// 	display_update();
+	// }
+	// while (i > 0){
+	// 	display_image(i, ufo);
+	// 	i--;
+	// 	display_update();
+	// }
+	// }
+	int q, w, e;
+	for (q=0; q<512 q++){
+	if(127<q<256){
+		gameMap[q]= 251;
+	}
+	if (255<q<384){
+		gameMap[q]=223;
+	}
+	else 
+		gameMap[q]= 255;
+	}
+	display_image(0,(const uint8_t) gameMap);
+	// for (w=(4+lane)){
+
+	// }
+	display_image(0, ufo);
+	display_update();
+
+	return 0;
+}
