afff7decb69c125600a5ea0ea7e6cf1ce0861e4e
Alvin Pettersson <alvinpe@kth.se>
2023-02-24 15:05:21 +0100

    Global variables

---
+++ hello dipsplay/porjectLib.h
@@ -0,0 +1,6 @@
+#include <pic32mx.h>
+#include <stdio.h>
+#include <stdint.h>
+
+uint8_t gameMap[512];
+volatile int charactersLane = 1; // the lane in which the character is in, starts at 1 which is middle, top is 0 and bottom is 2
