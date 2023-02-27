18ea75ebc0d706e70372524202eaa127036aa878
Alvin Pettersson <alvinpe@kth.se>
2023-02-27 15:47:10 +0100

    comments in projlib

--- hello dipsplay/projectlib.h
+++ hello dipsplay/projectlib.h
@@ -5,20 +5,29 @@
 */
 
 // declaration of sprites:
+
 extern const uint8_t const ufo[19];
 extern const uint8_t const spaceRock[10];
 
+
 // uint8_t gameMap[512];     //! probobly not needed
+
 int charactersLane; // the lane in which the character is in, starts at 128 which is middle, top is 0 and bottom is 256
 
+
 // checkio.c functions:
+
 int getbtns (void);
 int getsw (void);
 
+
 // display functions
+
 void showUfo(void);
 
+
 // init functions:
+
 void time2init();
 void time4init();
 void buttonInit();
