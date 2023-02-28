c17a74cb04568d625c287f20b9f08664163219ef
Chef-mux <ddmh89@gmail.com>
2023-02-28 10:15:19 +0100

    display functions update

--- hello dipsplay/display_functions.c
+++ hello dipsplay/display_functions.c
@@ -1,6 +1,12 @@
 
 //display_functions.c
 
+ #include <stdint.h> 
+ #include <pic32mx.h>
+ #include "projectlib.h"
+
+ volatile uint32_t delayTest;
+
 void move_ufo(int direction){ // will accept input to decide whiche direction to move. 
 
 	if (direction < 0){		//move upwards
@@ -39,7 +45,7 @@ void move_ufo(int direction){ // will accept input to decide whiche direction to
 			//display_update();
 			display_image(0, gameMap);
 
-			delayTest = 0;
+			delayTest = 0;      //! ska bytas mot timer och interrupt med counter
 			while (delayTest < 100000){
 				delayTest ++;
 			}
@@ -48,8 +54,8 @@ void move_ufo(int direction){ // will accept input to decide whiche direction to
 	}
 	if (direction > 0){		//move downwards
 		int timeOutCount = 0;
-		int blanksBelow = 254;	// to create blanks below the ship as it leaves the lane
-		int blanksAbove = 128;	// to create blanks above the ship as it spawns in
+		int blanksBelow = 254;	// to create blanks below the ship as itspawns  
+		int blanksAbove = 1;	// to create blanks above the ship as it leaves the lane
 		int shiftAbove = 1;		// how much to shift the ufo template when spawning
 		int shiftBelow = 7;		// how much to shift the ufo template when despawing
 		int j = 8;				// increment the outer loop
@@ -64,19 +70,16 @@ void move_ufo(int direction){ // will accept input to decide whiche direction to
 
 			for (i = 0; i < 19; i++){ // this for loop fills our temp array with a partial image of the ufo. more with every itteration.
 				temp0[i] = ((ufo[i] << shiftAbove) | blanksAbove);
-				temp1[i] = ((ufo[i] >> shiftBelow)| blanksBelow);	
+				temp1[i] = ((ufo[i] >> shiftBelow) | blanksBelow);	
 			}
 
 			int w = 0;
 			for( w ; w < 19 ; w++){	// this for loop copies out temp value into the map at the right lane
-				gameMap[((characterLane - 1)*128) + (10 + w)] = ((gameMap[((characterLane - 1)*128) + (10 + w)] | 255/* or 255 to reset the image*/ )
-				& temp0[w]);
-
-				
-				gameMap[((characterLane + 1)*128) + (10 + w)] = ((gameMap[((characterLane + 1)*128) + (10 + w)] | 255 )	& temp1[w]);
+				gameMap[((characterLane - 1)*128) + (10 + w)] = ((gameMap[((characterLane - 1)*128) + (10 + w)] | 255 )	& temp0[w]);
+				gameMap[((characterLane)*128) + (10 + w)] = ((gameMap[((characterLane)*128) + (10 + w)] | 255 )	& temp1[w]);
 			} 
 			blanksAbove = ((blanksAbove*2)+1); 	// decrements how much blank space should be used above
-			blanksBelow = (blanksBelow*2);	// increments how much blank space should be used below 
+			blanksBelow = ((blanksBelow*2)- 256);	// increments how much blank space should be used below 
 			shiftAbove ++;						// decrements how much of the ufo template to remove above
 			shiftBelow --;						// increments how much of the ufo template to remove below
 			j --;								// counts itterations. 
@@ -84,8 +87,8 @@ void move_ufo(int direction){ // will accept input to decide whiche direction to
 			//display_update();
 			display_image(0, gameMap);
 
-			delayTest = 0;
-			while (delayTest < 100000){
+			delayTest = 0;  //! ska bytas mot timer och interrupt med counter
+			while (delayTest < 1000000){
 				delayTest ++;
 			}
 		}
@@ -101,7 +104,7 @@ void showUfo(void){	// funktion för att ladda in ufot i game map.
 	
 	//display_update();
 	display_image(0, gameMap);
-    return:
+    return;
 }
 
 void setup_gameMap(void){
@@ -111,7 +114,53 @@ void setup_gameMap(void){
 		for(column = 0 ; column < 128 ; column++){			
 				gameMap[(row*128) + column] = 255;		
 		}
-	display_update();
-	showUfo();
 	}
+}
+
+void spawn_obstacle (int lane){ // spawns a spaceRock at the end of the map
+    int i = 0;
+	int column;
+	for(column = 127 ; column > 117 ; column--){			
+		gameMap[(lane*128) + column] = gameMap[((lane*128) + column) | 255] & spaceRock[i];
+        i++;
+
+        display_image(0, gameMap);
+
+		delayTest = 0;      //! ska bytas mot timer och interrupt med counter
+		while (delayTest < 100000){
+			delayTest ++;	
+	    }   
+    }
+}
+
+void explode(int lane){ //! testa funktionen
+
+    uint8_t temp[10][10];
+    int i;
+    int j;
+	for (i =;)
+    for(i = 0; i < 7; i++){
+        if(i % 2 == 0){
+            for(j = 0; j < 10; j++){
+                gameMap[(lane*128) + j] = (gameMap[(lane*128) + j] |255) & exp1[i][j];
+        }
+            display_image(0, gameMap);
+
+            delayTest = 0;      //! ska bytas mot timer och interrupt med counter
+            while (delayTest < 100000){
+                delayTest ++;	
+	    }
+    }  
+        else{
+            for(j = 0; j < 10; j++){
+                    gameMap[(lane*128) + j] = (gameMap[(lane*128) + j] |255) & (exp1[i][j] & exp1[i+1][j]);
+            }
+            display_image(0, gameMap);
+
+            delayTest = 0;      //! ska bytas mot timer och interrupt med counter
+            while (delayTest < 100000){
+                delayTest ++;	
+            }
+        } 
+    }
 }
--- hello dipsplay/projectlib.h
+++ hello dipsplay/projectlib.h
@@ -7,10 +7,12 @@
 // declaration of sprites:
 extern const uint8_t const ufo[19];
 extern const uint8_t const spaceRock[10];
-static const uint8_t const font[];
+extern const uint8_t const exp1[10];
+extern const uint8_t const exp2[10];
+extern const uint8_t const exp3[10];
+extern const uint8_t const exp4[10];
 
-// uint8_t gameMap[512];     //! probobly not needed
-int charactersLane; // the lane in which the character is in, starts at 128 which is middle, top is 0 and bottom is 256
+int characterLane; // the lane in which the character is in, starts at 128 which is middle, top is 0 and bottom is 256
 
 // checkio.c functions:
 int getbtns (void);
@@ -19,8 +21,10 @@ int getsw (void);
 // display functions
 
 void showUfo(void);
-void move_ufo(int direction)
-void setup_gameMap(void)
+void move_ufo(int direction);
+void setup_gameMap(void);
+void spawn_obstacle(int lane);
+uint8_t gameMap[512];		// array for the basic map
 
 // init functions:
 void timer2init();
